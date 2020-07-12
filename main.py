00d72be3468a2621ba8861e9c0da5529842914c2
Ervin Lee <65282792+perxpective@users.noreply.github.com>
2020-07-12 17:05:11 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-import replit
 import binascii
 output = ''
 
 print('[1] convert to binary')
 print('[2] covnert to hexadecimals')
 print('[3] convert to octodecimals')
-print('[4] decode to text')
+print('[4] decode to hexadecimal to text')
 option = input('select option: ')
 while not (option == '1' or option == '2' or option == '3' or option == '4'):
     print('invalid input')
@@ -14,16 +13,17 @@ while not (option == '1' or option == '2' or option == '3' or option == '4'):
 # text to ascii to binary
 if option == '1':
     string = input('enter string to cipher: ')
-    for char in range(len(string)):
-        char_num = bin(ord(string[char]))
-        output += char_num[2:]
+    for char in string:
+      char_num = bin(ord(char))
+      output += char_num[2:]
 
 # text to ascii to hexadecimals
 elif option == '2':
     string = input('enter string to cipher: ')
-    for char in range(len(string)):
-        char_num = hex(ord(string[char]))
-        output += char_num[2:]
+    for char in string:
+      char_num = hex(ord(char))
+      output += char_num[2:]
+
 
 # text to ascii to octodecimals
 elif option == '3':
@@ -34,33 +34,7 @@ elif option == '3':
 
 # deciphering
 elif option == '4':
-    print('- put 0x if hexadecimal')
-    print('- put 0b if binary')
-    print('- put 0o if octodecimal')
-    string = input('enter string to decipher: ')
-    while not ('0x' in string or '0b' in string or '0o' in string):
-        print('oops! something went wrong.')
-        string = input('enter string to decipher[minimum 8 chars]: ')
-
-    if '0b' in string:
-        string = string.replace("0b", "")
-        bin_values = string.split()
-        output = ""
-        for bin_value in bin_values:
-            integer = int(bin_value, 2)
-            char = chr(integer)
-            output += char
-            
-
-
-    elif '0x' in string:
-        string = int(string, 2)
-        output = bytearray.fromhex(string).decode('utf-8')
-
-    elif '0o' in string:
-        output.replace("0o","")
-        output += chr(int(string[0:4],8))
-        for char in range(1,len(string)//3+1):
-            output += chr(int(string[char*3+1:char*3+4], 8))
+  string = input('enter string to decipher: ')
+  output = binascii.unhexlify(string).decode()
 
 print('value: ', output)
